3bdc0177e06f0dee10294d4e180519ea9834c505
SumedhaJoshSoft <sumedha.jagtap@joshsoftware.com>
2022-11-29 12:50:41 +0530

    removed unnecessary checksiteshandler

--- main.go
+++ main.go
@@ -96,16 +96,8 @@ type Websites struct {
 }
 
 /*
-api handler working as a server context to handle below GET and POST api requests
-1. URL : /websites?name=""
-	Method : GET
-	Response :
-
-				http://www.google.com - UP
-				http://www.facebook.com - UP
-				http://www.fakewebsite1.com - DOWN
-
-2. URL : /websites
+api handler working as a server context to handle below POST api requests
+URL : /websites
 	Method : POST
 	Request Body :
 				{"websites":["http://www.google.com","http://www.facebook.com","http://www.fakewebsite1.com"]}
@@ -113,7 +105,7 @@ api handler working as a server context to handle below GET and POST api request
 				Websites updated successfully.
 	Updates memory map of websites with website statuses
 */
-func postWebsitesHandler(w http.ResponseWriter, req *http.Request) {
+func loadWebsitesHandler(w http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
 	log.Println("websitesHandler Handler started")
 	defer log.Println("websitesHandler Handler ended")
@@ -147,7 +139,17 @@ func postWebsitesHandler(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func getWebsitesHandler(w http.ResponseWriter, req *http.Request) {
+/*
+api handler working as a server context to handle below GET api requests
+1. URL : /websites?name=""
+	Method : GET
+	Response :
+
+				http://www.google.com - UP
+				http://www.facebook.com - UP
+				http://www.fakewebsite1.com - DOWN
+*/
+func checkSiteStatusHandler(w http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
 	log.Println("websitesHandler Handler started")
 	defer log.Println("websitesHandler Handler ended")
@@ -186,48 +188,6 @@ type Website struct {
 	Website string `json:"website"`
 }
 
-/*
-api handler working as a server context to handle below POST api request
-1. URL : /checksitestatus
-	Method : POST
-	Request Body :
-				{"website":"http://www.dsccddss.com"}
-	Response:
-				Site http://www.dsccddss.com is DOWN
-
-	Check the status of website and returns it
-*/
-func checksitestatusHandler(w http.ResponseWriter, req *http.Request) {
-	ctx := req.Context()
-	log.Println("checksitestatusHandler Handler started")
-	defer log.Println("checksitestatusHandler Handler ended")
-	select {
-	case <-ctx.Done():
-		err := ctx.Err()
-		log.Print("Ctx done checksitestatusHandler : ", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	default:
-
-		ctxAsClient := context.Background()
-
-		website := Website{}
-
-		err := json.NewDecoder(req.Body).Decode(&website)
-		if err != nil {
-			fmt.Fprint(w, "Error while updating websites.")
-		} else {
-			httpChecker := httpChecker{}
-			_, err := httpChecker.Check(ctxAsClient, website.Website)
-			if err != nil {
-				websitesMap[website.Website] = "DOWN"
-			} else {
-				websitesMap[website.Website] = "UP"
-			}
-			fmt.Fprintf(w, "Site %s is %s", website.Website, websitesMap[website.Website])
-		}
-	}
-}
-
 var websitesMap = make(map[string]string)
 
 func main() {
@@ -238,11 +198,9 @@ func main() {
 
 	router := mux.NewRouter()
 
-	//ping
 	router.HandleFunc("/", defaultHandler).Methods(http.MethodGet)
-	router.HandleFunc("/websites", getWebsitesHandler).Methods(http.MethodGet)
-	router.HandleFunc("/websites", postWebsitesHandler).Methods(http.MethodPost)
-	router.HandleFunc("/checksitestatus", checksitestatusHandler).Methods(http.MethodPost)
+	router.HandleFunc("/websites", checkSiteStatusHandler).Methods(http.MethodGet)
+	router.HandleFunc("/websites", loadWebsitesHandler).Methods(http.MethodPost)
 	srv := &http.Server{
 		Handler: router,
 		Addr:    "127.0.0.1:8000",
